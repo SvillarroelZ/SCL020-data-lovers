c8efaec7fa47cbd46dba70b034459d8d22b9e713
SvillarroelZ <sofiavillarroelzamora@gmail.com>
2022-05-03 11:20:56 -0400

    info e imagenes

--- src/main.js
+++ src/main.js
@@ -4,3 +4,44 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+//intento1
+// function rolChoice() {
+//     if(document.getElementById("assa").checked == true){
+//         document.getElementById("assassin").style.display = 'block';
+//         document.getElementById("fighter").style.display = 'none';
+//         document.getElementById("marksman").style.display = 'none';
+//         document.getElementById("mage").style.display = 'none';
+//         document.getElementById("tank").style.display = 'none';
+//         document.getElementById("support").style.display = 'none';
+//     }
+// }
+
+
+//intento2
+// function displayImage(rol) {
+//   var imgs = document.querySelectorAll(".cRol > img");
+
+//   for (var i = 0; i < imgs.length; i++) {
+//       imgs[i].style.display = "none";
+//   }
+//   document.getElementById(id).style.display = "inline";
+// }
+  
+
+//intento3
+
+var lastUnique;
+function displayImage(id, unique) {
+    var el = document.getElementById(id);
+
+    if (unique) {
+        if (lastUnique) {
+            lastUnique.style.display = 'none';
+        }
+
+        lastUnique = el;
+    }
+
+    el.style.display = 'block';
+}
