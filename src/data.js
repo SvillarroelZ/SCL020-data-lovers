4eb91efe08a52d4aa2148001e67c4fb42c65a47b
Constanza <c.leytonleiva@gmail.com>
2022-05-20 10:52:05 -0400

    Cambios trabajados con daniel

--- src/data.js
+++ src/data.js
@@ -1,32 +1,54 @@
+import data from "./data/lol/lol.js";
 export const getData = (data) => {
 };
+
 export const filterByRole = (rolChoice, allData) =>{
-    return allData.filter(campeon => campeon.tags.includes(rolChoice))
+    let newArray = normalizeInfo(allData)
+    return newArray.filter(campeon => campeon.tags.includes(rolChoice))
 };
 
 export const sortByDifficulty = (difficultyChoice, allData) =>{
     return allData.sort(function (a,b){return a-b;})
 };
 
-
 export const soSelecFunc = (newData, sortSelected) => {
     //let newData1 = [...newData] /*??????*/
-    
+    let sortedChars;
     if (sortSelected === 'sortA'){
-        const sortedCharA = newData.sort((a, b) => {
-            if (a.diffId < b.diffId) return -1;
+        sortedChars = newData.sort((a, b) => {
+            if (a.diffId < b.diffId)
+                return -1;
             return 1;
         });
-        console.log(sortedCharA)
-        return showCards1(sortedCharA);
     }
     
     else if (sortSelected === 'sortD') {
-        const sortedCharD = newData.sort((a, b) => {
-            if (a.diffId < b.diffId) return 1;
+        sortedChars = newData.sort((a, b) => {
+            if (a.diffId < b.diffId)
+                return 1;
             return -1;
-        });
-        
-    }
-    
+        });   
+    }  
+
+    return sortedChars;
+};
+
+function normalizeInfo() {
+    let newArr= [];
+    const allData = Object.values(data.data);
+    //console.log(data)
+    allData.forEach(element => {
+        newArr.push({
+            attack: element.info.attack,
+            defense: element.info.defense,
+            magic: element.info.magic,
+            difficulty: element.info.difficulty,
+            img: element.img
+        })
+
+       // newArr.append(element)
+    });
+    return newArr;
 }
+let infoN = normalizeInfo()
+console.log(infoN);
