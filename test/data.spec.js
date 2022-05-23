58153856ca3ca200df2436994e38ba360fda9aba
SvillarroelZ <102156826+SvillarroelZ@users.noreply.github.com>
2022-05-23 13:23:04 -0400

    test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,69 @@
-import { example, anotherExample } from '../src/data.js';
+import { describe, it } from 'eslint/lib/rule-tester/rule-tester';
+import { TestWatcher } from 'jest';
+import { filterByRole, soSelecFunc } from '../src/data.js';
 
+let saveData = {
+  Aatrox: {
+    name: "Aatrox",
+    info: {attack: 8, defense: 4, magic: 3, difficulty: 4},
+    tags: ["Fighter", "Tank"],
+  },
+  Ahri: {
+    name: "Ahri",
+    info: {attack: 3, defense: 4, magic: 8, difficulty: 5},
+    tags: ["Mage", "Assassin"],
+  },
+  Akali: {
+    name: "Akali",
+    info: {attack: 5, defense: 3, magic: 8, difficulty: 7},
+    tags: ["Assassin"],
+  },
+} 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+const allData = Object.values(saveData)
+
+/////////
+describe("test for filterbyRole", () => {
+  it("test if it's a function", () => {
+    expect(typeof filterByRole).toBe("function");
   });
+});
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+describe("test for soSelecFunc", () => {
+  it("test if it's a function", () => {
+    expect(typeof soSelecFunc).toBe("function");
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
+//////////
+
+
+describe('Función que filtra la data, para obtener los roles', () => {
+
+  it('Filter function', () => {
+    
+
+  const rol = "Assassin"
+  const resultRol = { 
+    Ahri: {
+    name: "Ahri",
+    tags: ["Mage", "Assassin"],
+  },
+    Akali: {
+    name: "Akali",
+    tags: ["Assassin"],
+  }, 
+
+}
+  let filtered = filterByRole (allData, rol)
+    expect(filtered).toEqual(resultRol);
+    
+});
+
+
+
+
+
 });
