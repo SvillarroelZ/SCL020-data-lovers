1c1493edf466da6d822af9481027c6bcae508687
Constanza <c.leytonleiva@gmail.com>
2022-05-24 16:03:33 -0400

    ultimas modificaciones test

--- test/data.spec.js
+++ test/data.spec.js
@@ -2,6 +2,42 @@ import { describe, it } from 'eslint/lib/rule-tester/rule-tester';
 import { TestWatcher } from 'jest';
 import { filterByRole, soSelecFunc, } from '../src/data.js';
 
+let sortedA = {
+  Aatrox: {
+    name: "Aatrox",
+    info: {attack: 8, defense: 4, magic: 3, difficulty: 4},
+    tags: ["Fighter", "Tank"],
+  },
+  Ahri: {
+    name: "Ahri",
+    info: {attack: 3, defense: 4, magic: 8, difficulty: 5},
+    tags: ["Mage", "Assassin"],
+  },
+  Akali: {
+    name: "Akali",
+    info: {attack: 5, defense: 3, magic: 8, difficulty: 7},
+    tags: ["Assassin"],
+  },
+} 
+const sortA = Object.values(sortedA)
+let sortedD = {
+  Akali: {
+    name: "Akali",
+    info: {attack: 5, defense: 3, magic: 8, difficulty: 7},
+    tags: ["Assassin"],
+  },
+  Ahri: {
+    name: "Ahri",
+    info: {attack: 3, defense: 4, magic: 8, difficulty: 5},
+    tags: ["Mage", "Assassin"],
+  },
+  Aatrox: {
+    name: "Aatrox",
+    info: {attack: 8, defense: 4, magic: 3, difficulty: 4},
+    tags: ["Fighter", "Tank"],
+  },
+} 
+const sortD = Object.values(sortedD)
 let saveData = {
   Aatrox: {
     name: "Aatrox",
@@ -27,36 +63,27 @@ describe("test for filterbyRole", () => {
   it("test if it's a function", () => {
     const rolChoice = "Assassin";
     const funrol = filterByRole(rolChoice, allData);
-    expect(typeof filterByRole).toBe("function");
-    expect(funrol).toContain(allData[2])
+    expect(typeof filterByRole).toBe("function");//espera que filterByRole sea una funcion
+    expect(funrol).toContain(allData[2]);//espera que el filtro por rol se aplique segun la eleccion del usuario
   });
-  it("should return Mage", () => {
-    const item = [saveData[0]];
-    const filter = "Mage";
-    expect(filter).toEqual("Mage");
+})
+
+describe("test for soSelecFunc", () => {
+  it("test if it's a function", () => {
+    const sortSelected= "sortA";
+    const funsort = soSelecFunc(allData, sortSelected);
+    expect(funsort).toEqual(sortA);
+  });
+  it("test if it's a function", () => {
+    const sortSelected= "sortD";
+    const funsort = soSelecFunc(allData, sortSelected);
+    expect(funsort).toEqual(sortD);
   });
 });
-
-
-
 describe("test for soSelecFunc", () => {
   it("test if it's a function", () => {
     expect(typeof soSelecFunc).toBe("function");
   });
 });
 
-
-
 //////////
-
-    describe('Función que filtra la data, para obtener los roles', () => {
-
-      it('Filter function', () => {
-      const rol = "Assassin";
-      const resultRol = [allData[1]];
-      const filterInfo= filterByRole(rol,resultRol);
-      expect(filterInfo).toEqual(resultRol);
-    
-    });
-    }) 
-
